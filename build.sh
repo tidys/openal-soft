b553f23dc80ca09e568bee1358a4ddf602a9484f
cc <xu_yanfeng@126.com>
2023-03-13 14:17:47 +0800

    release

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ export PATH=$PATH:$compiler # :$sysroot:$platform
 echo $PATH
 cmake ..    -DCMAKE_TOOLCHAIN_FILE=../XCompile-Android.txt \
             -DHOST=arm-linux-androideabi \
-        #    -DCMAKE_C_FLAGS="-std=c99 -march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16 -ffunction-sections -funwind-tables -fstack-protector-strong -fPIC -Wall -Wa,--noexecstack -Wformat -Werror=format-security -no-canonical-prefixes -DANDROID -DPLATFORM_ANDROID -D__ANDROID_API__=16"
+            -DCMAKE_BUILD_TYPE=Release
 make
  
