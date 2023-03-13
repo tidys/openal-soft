ac0a19cab6087f3472391fb8160b821928b8d2e6
ubuntu-build <xu_yanfeng@126.com>
2023-03-13 02:15:37 +0000

    1.19版本build

---
+++ build-android-r14b-119.sh
@@ -0,0 +1,23 @@
+rm -rf ./build
+mkdir ./build
+cd ./build
+# ndk_dir=$(pwd)/android-ndk-r14b
+# dir="/home/ubuntu/proj/openal-soft/android-ndk-r10e/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin"
+# export PATH=$PATH:$dir
+# cmake ..    -DCMAKE_TOOLCHAIN_FILE=../XCompile-Android.txt \
+#             -DHOST=aarch64-linux-android
+
+# dir="/home/ubuntu/proj/openal-soft/android-ndk-r10e/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin"
+# export PATH=$PATH:$dir
+# cmake ..    -DCMAKE_TOOLCHAIN_FILE=../XCompile-Android.txt \
+#             -DHOST=arm-linux-androideabi
+
+# dir="/home/ubuntu/proj/openal-soft/android-ndk-r15c/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin"
+# export PATH=$PATH:$dir
+# cmake ..    -DCMAKE_TOOLCHAIN_FILE=../XCompile-Android.txt \
+#             -DHOST=arm-linux-androideabi
+
+# dir="/home/ubuntu/proj/openal-soft/android-ndk-r18b/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin"
+# export PATH=$PATH:$dir
+# cmake ..    -DCMAKE_TOOLCHAIN_FILE=../XCompile-Android.txt \
+#             -DHOST=arm-linux-androideabi
