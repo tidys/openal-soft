6fe0a0c1480e89a380c32f3a2b1715824e042d82
ubuntu-build <xu_yanfeng@126.com>
2023-03-13 02:57:06 +0000

    1

--- build.sh
+++ build.sh
@@ -4,5 +4,10 @@ mkdir ./build
 cd ./build
 
 echo $ndk_root
-cmake .. -DANDROID_STL=c++_shared -DANDROID_LD=lld \
+cmake .. \
+    -G"Unix Makefiles" \
+    -DANDROID_STL=c++_shared \
+    -DANDROID_LD=lld \
+    -D__ANDROID_API__=21 \
+    -DANDROID_ABI=armeabi-v7a \
     -DCMAKE_TOOLCHAIN_FILE=${ndk_root}/build/cmake/android.toolchain.cmake
