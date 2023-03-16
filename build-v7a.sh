22dcf218ceace1a43447fc54754814767874cc2c
ubuntu-build <xu_yanfeng@126.com>
2023-03-16 06:20:59 +0000

    1

--- build-v7a.sh
+++ build-v7a.sh
@@ -4,7 +4,7 @@ mkdir ./build
 cd ./build
 compiler=$ndk_dir/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin
 sysroot=$ndk_dir/sysroot
-platform=$ndk_dir/platforms/android-9/arch-arm/
+platform=$ndk_dir/platforms/android-21/arch-arm/
 export PATH=$PATH:$compiler:$sysroot:$platform
 # echo $PATH
 
@@ -15,9 +15,11 @@ export PATH=$PATH:$compiler:$sysroot:$platform
 
 cmake ..    -DCMAKE_TOOLCHAIN_FILE=../XCompile-Android.txt \
             -DANDROID_ABI="armeabi-v7a" \
+            -DHOST=arm-linux-androideabi \
+            -DCMAKE_SYSROOT=$platform \
             -DANDROID_NDK=$ndk_dir \
             -DCMAKE_BUILD_TYPE=Release \
-            -DANDROID_PLATFORM=android-22 \
+            -DANDROID_PLATFORM=android-21 \
 
 make
 tar -cvf lib.zip ./libopenal.so # ./libopenal.so.1 ./libopenal.so.1.19.1
