95799c86f1c96d47029eb61677f77b1aaa32678d
ubuntu-build <xu_yanfeng@126.com>
2023-03-16 08:23:27 +0000

    1

--- build-v8a.sh
+++ build-v8a.sh
@@ -2,21 +2,20 @@ ndk_dir=$(pwd)/android-ndk-r14b
 rm -rf ./build
 mkdir ./build
 cd ./build
-compiler=$ndk_dir/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin
+compiler=$ndk_dir/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin
 sysroot=$ndk_dir/sysroot
-platform=$ndk_dir/platforms/android-21/arch-arm/
+platform=$ndk_dir/platforms/android-21/arch-arm64/
 export PATH=$PATH:$compiler:$sysroot:$platform
 
 cmake ..    -DCMAKE_TOOLCHAIN_FILE=../XCompile-Android.txt \
-            -DANDROID_ABI="armeabi-v7a" \
-            -DHOST=arm-linux-androideabi \
+            -DANDROID_ABI="arm64-v8a" \
+            -DHOST=aarch64-linux-android \
             -DCMAKE_SYSROOT=$platform \
             -DANDROID_NDK=$ndk_dir \
             -DCMAKE_BUILD_TYPE=Release \
             -DANDROID_PLATFORM=android-21 \
-            -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-z,nodlopen" \
 
 make
-tar -cvf lib.zip ./libopenal.so # ./libopenal.so.1 ./libopenal.so.1.19.1
+tar -cvf lib.zip ./libopenal.so  ./libopenal.so.1 ./libopenal.so.1.19.1
 gzip lib.zip
 echo "if you use vscode, please download lib.zip"
