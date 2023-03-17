7af9f8f1cf5a2bd47de00359e9705d7d42d9de1c
ubuntu-build <xu_yanfeng@126.com>
2023-03-17 05:36:57 +0000

    1

--- build.sh
+++ build.sh
@@ -1,13 +1,41 @@
-ndk_root=$(pwd)/android-ndk-r14b
-rm -rf ./build
-mkdir ./build
-cd ./build
+ndk_root=$(pwd)/android-ndk-r20b
+prefix_root=$(pwd)/android
+rm -rf $prefix_root
+version=1.22.0
+prefix_root+=$version
 
-echo $ndk_root
-cmake .. \
-    -G"Unix Makefiles" \
-    -DANDROID_STL=c++_shared \
-    -DANDROID_LD=lld \
-    -D__ANDROID_API__=21 \
-    -DANDROID_ABI=armeabi-v7a \
-    -DCMAKE_TOOLCHAIN_FILE=${ndk_root}/build/cmake/android.toolchain.cmake
+for abi in arm64-v8a # armeabi-v7a
+do 
+    for build_type in Debug # Release MinSizeRel
+    do
+        echo begin build [$abi / $build_type] ...
+        prefix=$prefix_root/$abi/$build_type
+        rm -rf ./build
+        mkdir ./build
+        cd ./build
+        cmake .. \
+            -DCMAKE_BUILD_TYPE=$build_type \
+            -DANDROID_STL=c++_shared \
+            -DANDROID_ABI=$abi \
+            -DCMAKE_INSTALL_PREFIX=$prefix \
+            -DANDROID_PLATFORM=android-21 \
+            -DCMAKE_TOOLCHAIN_FILE=${ndk_root}/build/cmake/android.toolchain.cmake
+
+
+
+        make
+        if [ $? -ne 0 ]; then
+            echo make failed [$abi / $build_type] ...
+            exit
+        fi
+
+        make install
+        if [ $? -ne 0 ]; then
+            echo make install failed [$abi / $build_type] ...
+            exit
+        fi
+
+        cd ..
+        echo build success [$abi / $build_type] ...
+    done
+done
