72a3d26f2e6f68079c94507d769c6e29f9cb848d
ubuntu-build <xu_yanfeng@126.com>
2023-03-17 05:52:13 +0000

    使用ndk r22b变成成功了

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-ndk_root=$(pwd)/android-ndk-r20b
+ndk_root=$(pwd)/android-ndk-r22b
 prefix_root=$(pwd)/android
 rm -rf $prefix_root
 version=1.22.0
 prefix_root+=$version
 
-for abi in arm64-v8a # armeabi-v7a
+for abi in arm64-v8a armeabi-v7a
 do 
-    for build_type in Debug # Release MinSizeRel
+    for build_type in Debug Release MinSizeRel
     do
         echo begin build [$abi / $build_type] ...
         prefix=$prefix_root/$abi/$build_type
