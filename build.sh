59b2fdbaa6bfa15ae324e83171d4b4a3a99fdbb9
ubuntu-build <xu_yanfeng@126.com>
2023-03-20 01:07:53 +0000

    调整构建脚本

--- build.sh
+++ build.sh
@@ -25,16 +25,29 @@ build(){
             echo "not exist: $toolchains"
             exit 
         fi
+
+        # export PATH=$PATH:$sysroot/usr/lib
+            # -DANDROID_STL=c++_shared \
+            # -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE \
+            # -CMAKE_INSTALL_RPATH=$sysroot/usr/lib \
+            # -DCMAKE_PREFIX_PATH=$sysroot/usr/lib \
+            # -DCMAKE_FIND_USE_CMAKE_PATH=TRUE \
+            # -DCMAKE_LIBRARY_PATH=$sysroot/usr/lib \
+            # -DCMAKE_RC_COMPILER=$toolchains/$toolchains_prefix-windres \
+
+            # -DCMAKE_CXX_COMPILER="/home/ubuntu/proj/openal-soft/android-ndk-r20b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android21-clang++" \
+            # -DCMAKE_C_COMPILER="/home/ubuntu/proj/openal-soft/android-ndk-r20b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android21-clang" \
+            # -DCMAKE_SYSROOT="/home/ubuntu/proj/openal-soft/android-ndk-r20b/sysroot" \
+            # -DANDROID_LD=lld \
+            # -DCMAKE_TOOLCHAIN_FILE=$ndk_root/build/cmake/android.toolchain.cmake \
         cmake .. \
             -DCMAKE_SYSTEM_NAME=Android \
-            -DANDROID_STL=c++_shared \
-            -DANDROID_LD=lld \
+            -DCMAKE_HOST_SYSTEM=Linux \
             -DANDROID_ABI=$abi \
+            -DCMAKE_INSTALL_PREFIX=$install_prefix \
             -DCMAKE_C_COMPILER=$toolchains/$toolchains_prefix-gcc \
             -DCMAKE_CXX_COMPILER=$toolchains/$toolchains_prefix-g++ \
-            -DCMAKE_RC_COMPILER=$toolchains/$toolchains_prefix-windres \
-            -DCMAKE_INSTALL_PREFIX=$install_prefix \
-            -DCMAKE_SYSROOT=$sysroot $sysroot/usr/lib \
+            -DCMAKE_SYSROOT=$sysroot \
             -DANDROID_NDK=$ndk_root \
             -DCMAKE_BUILD_TYPE=$build_type \
             -DANDROID_PLATFORM=android-21 \
